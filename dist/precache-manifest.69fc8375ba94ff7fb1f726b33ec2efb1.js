bfd9cf28f96ba265241d55717d91cf2fe59f9e2a
Eben Gunadi <egunadi@usc.edu>
2023-12-01 08:53:49 -0800

    Rebuild dist

--- dist/precache-manifest.69fc8375ba94ff7fb1f726b33ec2efb1.js
+++ dist/precache-manifest.69fc8375ba94ff7fb1f726b33ec2efb1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b339cb1cd8385cc7b735",
+    "revision": "4c331edf7c5f474925f4",
     "url": "/dsci554-project/css/app.303d2ca5.css"
   },
   {
-    "revision": "b339cb1cd8385cc7b735",
-    "url": "/dsci554-project/js/app.7953c631.js"
+    "revision": "4c331edf7c5f474925f4",
+    "url": "/dsci554-project/js/app.17e46030.js"
   },
   {
     "revision": "8954f1ded5b28b4d2241",
@@ -32,19 +32,19 @@ self.__precacheManifest = [
     "url": "/dsci554-project/fonts/nucleo-icons.8224e016.eot"
   },
   {
-    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
-    "url": "/dsci554-project/fonts/nucleo-icons.ff208b8d.woff2"
-  },
-  {
     "revision": "b0dc05d015e91e7d28d79cd0056fe555",
     "url": "/dsci554-project/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/dsci554-project/fonts/nucleo-icons.ff208b8d.woff2"
   },
   {
     "revision": "ae42fa524981a56f3f289447cff36eb1",
     "url": "/dsci554-project/img/nucleo-icons.ae42fa52.svg"
   },
   {
-    "revision": "902755de049a8a7c1e9977efde43e095",
+    "revision": "f5b5a547b142b09c1b5f4bfa402350ea",
     "url": "/dsci554-project/index.html"
   },
   {
@@ -52,44 +52,36 @@ self.__precacheManifest = [
     "url": "/dsci554-project/.gitkeep"
   },
   {
-    "revision": "c6394e6dd5c01a22d4e581e68367962a",
-    "url": "/dsci554-project/Dashboard.png"
-  },
-  {
     "revision": "996d8248f580f8e26e6c45c67da9b5a6",
     "url": "/dsci554-project/favicon.png"
   },
   {
-    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
-    "url": "/dsci554-project/img/faces/face-2.jpg"
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/dsci554-project/img/faces/face-0.jpg"
   },
   {
-    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
-    "url": "/dsci554-project/img/faces/face-3.jpg"
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/dsci554-project/img/default-avatar.png"
   },
   {
     "revision": "99e58416b89637502b40ac8350eed85a",
     "url": "/dsci554-project/img/faces/face-1.jpg"
   },
   {
-    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
-    "url": "/dsci554-project/img/faces/face-5.jpg"
-  },
-  {
-    "revision": "bf1684a30a86ba1b222aab3acff16356",
-    "url": "/dsci554-project/img/faces/face-7.jpg"
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/dsci554-project/img/faces/face-3.jpg"
   },
   {
     "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
     "url": "/dsci554-project/img/faces/face-4.jpg"
   },
   {
-    "revision": "4b87e628f4ef9988718860890b2a682f",
-    "url": "/dsci554-project/img/faces/face-6.jpg"
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/dsci554-project/img/faces/face-2.jpg"
   },
   {
-    "revision": "a9615bac158705203261e8348f574cc8",
-    "url": "/dsci554-project/img/default-avatar.png"
+    "revision": "c6394e6dd5c01a22d4e581e68367962a",
+    "url": "/dsci554-project/Dashboard.png"
   },
   {
     "revision": "43b98081492ac3bcb4a1fac6cf709403",
@@ -100,12 +92,16 @@ self.__precacheManifest = [
     "url": "/dsci554-project/img/favicon.png"
   },
   {
-    "revision": "a9615bac158705203261e8348f574cc8",
-    "url": "/dsci554-project/img/faces/face-0.jpg"
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/dsci554-project/img/faces/face-7.jpg"
   },
   {
-    "revision": "f575a04ebbb31b5798a4c54783e745a2",
-    "url": "/dsci554-project/img/new_logo.png"
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/dsci554-project/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/dsci554-project/img/faces/face-6.jpg"
   },
   {
     "revision": "d27fbc90c2e644dfdc9765640dc713b9",
@@ -116,35 +112,39 @@ self.__precacheManifest = [
     "url": "/dsci554-project/img/loading-bubbles.svg"
   },
   {
-    "revision": "6be21e8a1b7d63048728851c6003e189",
-    "url": "/dsci554-project/img/sidebar-1.jpg"
-  },
-  {
-    "revision": "44bf13a71a4db6e15913fe8af9296711",
-    "url": "/dsci554-project/img/tim_80x80.png"
-  },
-  {
-    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
-    "url": "/dsci554-project/img/vue-logo.png"
+    "revision": "f575a04ebbb31b5798a4c54783e745a2",
+    "url": "/dsci554-project/img/new_logo.png"
   },
   {
     "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
     "url": "/dsci554-project/img/sidebar-2.jpg"
   },
   {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/dsci554-project/robots.txt"
+    "revision": "44bf13a71a4db6e15913fe8af9296711",
+    "url": "/dsci554-project/img/tim_80x80.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/dsci554-project/img/sidebar-1.jpg"
   },
   {
     "revision": "cd253e23ed052deeb80b42d2ed772183",
     "url": "/dsci554-project/img/sidebar-3.jpg"
   },
   {
-    "revision": "897b4cf909210560a84398d36da51983",
-    "url": "/dsci554-project/img/sidebar-4.jpg"
+    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
+    "url": "/dsci554-project/img/vue-logo.png"
   },
   {
     "revision": "9ebea76ee1225f00d882b21547a7b49f",
     "url": "/dsci554-project/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dsci554-project/robots.txt"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/dsci554-project/img/sidebar-4.jpg"
   }
 ];
